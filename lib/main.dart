0c996bb6280cb1e483877aca3222940fa0048205
unknown <huutam.ptnk@gmail.com>
2018-10-27 22:03:22 +0300

    modify main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,14 @@ class MyApp extends StatelessWidget {
         // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(title: Text('EasyList')),
+        body: Card(
+          child: Column(
+            children: <Widget>[Image(), Text('Food paradise')],
+          ),
+        ),
+      ),
     );
   }
 }
